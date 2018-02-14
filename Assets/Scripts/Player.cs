cf18b211874e7170aec45c3dca6bb25b263006f9
Sarah Herzog <sarahherzog@fife.ac.uk>
2018-02-14 14:57:39 +0000

    Player script now has left/right movement, health, and damage function.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour 
+{
+	#region Variables
+	// Exposed Variables
+	public float m_health = 100;
+	public float m_speed = 10;
+	#endregion
 
-    // Use this for initialization
-    void Start() {
-        Debug.Log("Hello world!");
-    }
-	
-	// Update is called once per frame
-	void Update () {
-	
+	#region Public Functions
+	// Damage the player - may cause death!
+	public void Damage(float _damage)
+	{
+		m_health = m_health - _damage;
+
+		// TODO: Handle death!
 	}
+	#endregion
+
+	#region Unity Functions
+	void Update()
+	{
+		// Check if we are pressing a button to more horizontally
+		float horizontal = Input.GetAxis("Horizontal");
+		Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+		Vector2 velocity = rigidBody.velocity;
+		velocity.x = horizontal * m_speed;
+		rigidBody.velocity = velocity;
+		// TODO: Set animation parameter
+	}
+	#endregion
 }
