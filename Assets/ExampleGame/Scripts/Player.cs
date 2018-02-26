5a93aa4f3343e797eb0ffb34a5a633a9dc968b6e
Sarah Herzog <sarah.ann.herzog@gmail.com>
2018-02-26 12:51:59 +0000

    Added enemies and damage

--- Assets/ExampleGame/Scripts/Player.cs
+++ Assets/ExampleGame/Scripts/Player.cs
@@ -5,17 +5,27 @@ public class Player : MonoBehaviour {
 
     public string logText = "Hello world again";
     public float speed = 2;
+    public float health = 10;
 
     // Use this for initialization
-    void Start() {
-        
+    void Start()
+    {
+        Debug.Log(logText);
+        //ApplyDamage(1);
     }
 	
 	// Update is called once per frame
-	void Update () {
-        Debug.Log(logText);
+	void Update ()
+    {
         Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
         
         rigidBody.velocity = Vector2.right * speed;
     }
+
+    public void ApplyDamage (float damageToDeal)
+    {
+        health = health - damageToDeal;
+    }
 }
+
+// NO CODE HERE
