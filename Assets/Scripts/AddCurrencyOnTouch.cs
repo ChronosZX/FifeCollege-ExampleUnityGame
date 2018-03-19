de34b2f8ce7e794eb080e55484ece6ee4e39235b
Sarah Herzog <sarah.ann.herzog@gmail.com>
2018-03-19 12:20:37 +0000

    Cleaned up code, added new currency and item scripts

---
+++ Assets/Scripts/AddCurrencyOnTouch.cs
@@ -0,0 +1,74 @@
+﻿// *********************************************************************************************************************
+// File: AddCurrencyOnTouch.cs
+// Purpose: Adds currency to a CurrencyPouch that touches this trigger
+// Project: Fife College Unity Toolkit
+// Copyright Fife College 2018
+// *********************************************************************************************************************
+
+
+// *********************************************************************************************************************
+#region Imports
+// *********************************************************************************************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#endregion
+// *********************************************************************************************************************
+
+
+// *********************************************************************************************************************
+[RequireComponent(typeof(Collider2D))]
+public class AddCurrencyOnTouch : MonoBehaviour {
+	// *********************************************************************************************************************
+
+
+	// *****************************************************************************************************************
+	#region Variables
+	// *****************************************************************************************************************
+	// Exposed Variables
+	[Tooltip("What type of currency should be added?")]
+	public string m_currencyType = "score";
+	[Tooltip("How much currency will be added?")]
+	public int m_amount = 1;
+	[Tooltip("Should we destroy this object after collecting it?")]
+	public bool m_destroyWhenAdded = true;
+	[Tooltip("The sound that should be played when this currency is added")]
+	public AudioClip m_currencyAddSound = null;
+	#endregion
+	// *****************************************************************************************************************
+
+
+	// *****************************************************************************************************************
+	#region Unity Functions
+	// *****************************************************************************************************************
+	// When a trigger interaction starts involving this game object...
+	void OnTriggerEnter2D(Collider2D _other)
+	{
+		// Check if the other collider that we hit has a CurrencyPouch on it
+		CurrencyPouch currencyPouch = _other.GetComponent<CurrencyPouch>();
+		if (currencyPouch != null) {
+			// Attempt to add to the CurrencyPouch
+			bool added = currencyPouch.AddCurrency(m_currencyType, m_amount);
+
+			// if we successfully added it...
+			if (added) {
+				// If we should destroy this object, do so
+				if (m_destroyWhenAdded) {
+					Destroy (gameObject);
+				}
+
+				// If we have a sound to play, play it
+				if (m_currencyAddSound) {
+					// Play the sound for currency add at this locaiton
+					AudioSource.PlayClipAtPoint(m_currencyAddSound,transform.position);
+				}
+			}
+		}
+	}
+	// *****************************************************************************************************************
+	#endregion
+	// *****************************************************************************************************************
+
+
+}
+// *********************************************************************************************************************
--- Assets/Scripts/HealthPool.cs
+++ Assets/Scripts/HealthPool.cs
@@ -1,26 +1,68 @@
-﻿using System.Collections;
+﻿// *********************************************************************************************************************
+// File: HealthPool.cs
+// Purpose: A health pool that can be damaged by atacks
+// Project: Fife College Unity Toolkit
+// Copyright Fife College 2018
+// *********************************************************************************************************************
+
+
+// *********************************************************************************************************************
+#region Imports
+// *********************************************************************************************************************
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#endregion
+// *********************************************************************************************************************
 
-public class HealthPool : MonoBehaviour 
-{
+
+// *********************************************************************************************************************
+public class HealthPool : MonoBehaviour {
+// *********************************************************************************************************************
+
+
+	// *****************************************************************************************************************
 	#region Variables
+	// *****************************************************************************************************************
 	// Exposed Variables
+	[Header("Primary")]
+	[Tooltip("How much health this object has")]
 	public float m_health = 100;
+	[Tooltip("How long this object should be invulnerable after taking damage, in seconds")]
 	public float m_invulnDuration = 0;
+	[Tooltip("If true, this object will be destroyed when health reaches 0")]
 	public bool m_destroyOnDeath = true;
+	[Tooltip("The target type of the attack must match this type in order to deal damage")]
+	public string m_targetType = "enemy";
 
+	[Header("Effects")]
+	[Tooltip("The sound that should be played when this object takes damage")]
+	public AudioClip m_damageSound = null;
+	[Tooltip("The sound that should be played when this object dies")]
+	public AudioClip m_deathSound = null;
+	[Tooltip("The animator that should be used for damage animations")]
+	public Animator m_animator;
+	[Tooltip("The name of the trigger parameter that should be used for damage effects")]
+	public string m_damageTrigger;
+	[Tooltip("The name of the trigger parameter that should be used for death effects")]
+	public string m_deathTrigger;
+
+	// Private Variables
 	private float m_invulnEnd = 0;
 	#endregion
+	// *****************************************************************************************************************
 
+
+	// *****************************************************************************************************************
 	#region Public Functions
+	// *****************************************************************************************************************
 	// Damage the player - may cause death!
-	public void Damage(float _damage)
+	public bool Damage(float _damage, string _targetType)
 	{
 		// Don't deal damage if we are invulnerable
 		// (Check current time against what time it should be to
 		//    stop being invulnerable)
-		if (!IsInvulnerable())
+		if (!IsInvulnerable() && _targetType == m_targetType)
 		{
 			// Reduce our health by the damage taken
 			m_health = m_health - _damage;
@@ -29,9 +71,17 @@ public class HealthPool : MonoBehaviour
 			// This makes us become invulnerable for m_invulnDuration seconds
 			m_invulnEnd = Time.time + m_invulnDuration;
 
-			// TODO: Effects for taking damage
-			//		- Animation
-			//		- Sound
+			// If we have specified a sound to play on damage...
+			if (m_damageSound != null) {
+				// Play the sound for damage at this locaiton
+				AudioSource.PlayClipAtPoint(m_damageSound,transform.position);
+			}
+
+			// If we have supplied an animator and a trigger to use when damaged...
+			if (m_animator != null && m_damageTrigger != "") {
+				// Play death animation
+				m_animator.SetTrigger(m_damageTrigger);
+			}
 
 			// If our health has dropped to 0 or lower, we are dead!
 			if (m_health <= 0)
@@ -43,21 +93,40 @@ public class HealthPool : MonoBehaviour
 					Destroy(gameObject);
 				}
 
-				// TODO: Effects for death
-				//		- Animation
-				//		- Sound
-			}
-		}
-	}
+				// If we have specified a sound to play on death...
+				if (m_deathSound != null) {
+					// Play the sound for dying at this locaiton
+					AudioSource.PlayClipAtPoint(m_deathSound,transform.position);
+				}
 
+				// If we have supplied an animator and a trigger to use on death...
+				if (m_animator != null && m_deathTrigger != "") {
+					// Play death animation
+					m_animator.SetTrigger(m_deathTrigger);
+				}
+			}
+
+			// We dealt damage, so return true
+			return true;
+		}
+
+		// We did not deal damage, so return false
+		return false;
+	}
+	// *****************************************************************************************************************
 	public void Heal(float _healing)
 	{
 		m_health = m_health + _healing;
 	}
-
+	// *****************************************************************************************************************
 	public bool IsInvulnerable()
 	{
 		return Time.time < m_invulnEnd;
 	}
+	// *****************************************************************************************************************
 	#endregion
+	// *****************************************************************************************************************
+
+
 }
+// *********************************************************************************************************************
