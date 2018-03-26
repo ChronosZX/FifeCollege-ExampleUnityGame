df0066417c13717e40444f5ad631b119d36def17
Sarah Herzog <sarah.ann.herzog@gmail.com>
2018-03-26 13:02:56 +0100

    AddCurrencyOnDeath (not yet working)

--- Assets/Fife College Script Toolkit/Inventory/AddCurrencyOnDeath.cs
+++ Assets/Fife College Script Toolkit/Inventory/AddCurrencyOnDeath.cs
@@ -1,7 +1,6 @@
 ﻿// *********************************************************************************************************************
-// File: DestroyTileOnCollision.cs
-// Purpose: When the specified object collides with a tile in this map, 
-// 			destroy the tile
+// File: AddCurrencyOnDeath.cs
+// Purpose: Adds currency to a CurrencyPouch when this object dies
 // Project: Fife College Unity Toolkit
 // Copyright Fife College 2018
 // *********************************************************************************************************************
@@ -13,27 +12,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// We need to include the tilemap library
-using UnityEngine.Tilemaps;
 #endregion
 // *********************************************************************************************************************
 
 
 // *********************************************************************************************************************
-public class DestroyTileOnCollision : MonoBehaviour {
-	// *********************************************************************************************************************
+public class AddCurrencyOnDeath : MonoBehaviour {
+// *********************************************************************************************************************
 
 
 	// *****************************************************************************************************************
 	#region Variables
 	// *****************************************************************************************************************
 	// Exposed Variables
-	[Tooltip("The Tilemap we will check tiles for")]
-	public Tilemap m_tileMap;
-	[Tooltip("The Object which we will check collisions with")]
-	public Collider2D m_targetCollider;
-	[Tooltip("Optional particle effect to play when the tile is broken")]
-	public GameObject m_particlePrefab = null;
+	[Tooltip("What type of currency should be added?")]
+	public string m_currencyType = "score";
+	[Tooltip("How much currency will be added?")]
+	public int m_amount = 1;
+	[Tooltip("When this health pool dies, we'll add currency")]
+	public HealthPool m_healthPool;
+	[Tooltip("The CurrencyPouch to which this currency should be added")]
+	public CurrencyPouch m_currencyPouch = null;
 	#endregion
 	// *****************************************************************************************************************
 
@@ -41,38 +40,14 @@ public class DestroyTileOnCollision : MonoBehaviour {
 	// *****************************************************************************************************************
 	#region Unity Functions
 	// *****************************************************************************************************************
-	// When a collision interaction starts involving this game object...
-	void OnCollisionEnter2D(Collision2D _collision)
+	// Called once per frame
+	void Update()
 	{
-		// Only act if the object we collided with is our target
-		if (_collision.collider == m_targetCollider)
+		// If the health pool has died...
+		if (m_healthPool.IsAlive() == false)
 		{
-			// Create a vector variable to hold our hit position
-			Vector3 hitPosition = Vector3.zero;
-
-			// Loop through each collision hit position in the collision data
-			foreach (ContactPoint2D hit in _collision.contacts)
-			{
-				// Record teh hit location
-				hitPosition.x = hit.point.x + 0.1f * hit.normal.x;
-				hitPosition.y = hit.point.y + 0.1f * hit.normal.y;
-
-				// Get the tile's position in the grid
-				Vector3Int tilePosition = m_tileMap.WorldToCell(hitPosition);
-
-				// Set that tile to null (meaning delete it)
-				m_tileMap.SetTile(tilePosition, null);
-			}
-
-			// If we have a particle effect
-			if (m_particlePrefab != null)
-			{
-				// Use instantiate to create our particle prefab at our hit position
-				GameObject particles = Instantiate(m_particlePrefab);
-
-				// Move our particles to the correct location
-				particles.transform.position = _collision.collider.transform.position;
-			}
+			// Attempt to add to the CurrencyPouch
+			m_currencyPouch.AddCurrency(m_currencyType, m_amount);
 		}
 	}
 	#endregion
